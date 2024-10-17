dba0313d8b591a7db3e4af9a894b43920e6562dd
PragyanPandeyX <107480064+PragyanPandeyX@users.noreply.github.com>
2024-10-18 02:05:16 +0530

    Update bot.py

--- PragyanMusic/core/bot.py
+++ PragyanMusic/core/bot.py
@@ -6,7 +6,7 @@ import config
 from ..logging import LOGGER
 
 
-class AashikaMusicBot(Client):
+class PragyanMusic(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
