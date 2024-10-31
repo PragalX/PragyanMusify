42352b6c939472b17c613bbbd454fafcd25fdc20
AkhandaNandTripathi <168406488+PragalX@users.noreply.github.com>
2024-11-01 04:50:59 +0530

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -1,43 +1,34 @@
 import os
-import sys
 from typing import List
+
 import yaml
 
 languages = {}
-commands = {}
 languages_present = {}
 
-def get_command(value: str) -> List:
-    try:
-        return commands["command"][value]
-    except KeyError:
-        print(f"Command '{value}' not found in commands.yml.")
-        return []
 
 def get_string(lang: str):
-    return languages.get(lang, languages["en"])
+    return languages[lang]
 
-# Load commands from commands.yml
-try:
-    with open(r"./strings/commands.yml", encoding="utf8") as f:
-        commands = yaml.safe_load(f)
-except Exception as e:
-    print(f"Error loading commands.yml: {e}")
-    sys.exit()
 
-# Load languages from langs directory
 for filename in os.listdir(r"./strings/langs/"):
+    if "en" not in languages:
+        languages["en"] = yaml.safe_load(
+            open(r"./strings/langs/en.yml", encoding="utf8")
+        )
+        languages_present["en"] = languages["en"]["name"]
     if filename.endswith(".yml"):
         language_name = filename[:-4]
-        try:
-            with open(r"./strings/langs/" + filename, encoding="utf8") as f:
-                languages[language_name] = yaml.safe_load(f)
-            languages_present[language_name] = languages[language_name].get("name", language_name)
-        except Exception as e:
-            print(f"There is an issue with the language file '{filename}': {e}")
-            sys.exit()
-
-# Set default language if 'en' is missing
-if "en" not in languages:
-    print("English language file is missing in langs directory.")
-    sys.exit()
+        if language_name == "en":
+            continue
+        languages[language_name] = yaml.safe_load(
+            open(r"./strings/langs/" + filename, encoding="utf8")
+        )
+        for item in languages["en"]:
+            if item not in languages[language_name]:
+                languages[language_name][item] = languages["en"][item]
+    try:
+        languages_present[language_name] = languages[language_name]["name"]
+    except:
+        print("There is some issue with the language file inside bot.")
+        exit()
