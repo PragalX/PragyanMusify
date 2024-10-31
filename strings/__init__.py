ffbea3799f33cd7d2f4a0aa6a01ea5f0351248eb
AkhandaNandTripathi <168406488+PragalX@users.noreply.github.com>
2024-11-01 04:32:24 +0530

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -1,3 +1,4 @@
+#
 # Copyright (C) 2024 by IamDvis@Github, < https://github.com/IamDvis >.
 #
 # This file is part of < https://github.com/IamDvis/DV-MUSIC > project,
@@ -14,18 +15,18 @@ import yaml
 
 languages = {}
 commands = {}
+
 languages_present = {}
 
 
 def get_command(value: str) -> List:
-    return commands["command"].get(value, [])
+    return commands["command"][value]
 
 
 def get_string(lang: str):
-    return languages.get(lang, {})
+    return languages[lang]
 
 
-# Load commands from the strings directory
 for filename in os.listdir(r"./strings"):
     if filename.endswith(".yml"):
         language_name = filename[:-4]
@@ -33,7 +34,7 @@ for filename in os.listdir(r"./strings"):
             open(r"./strings/" + filename, encoding="utf8")
         )
 
-# Load languages from the langs directory
+
 for filename in os.listdir(r"./strings/langs/"):
     if "en" not in languages:
         languages["en"] = yaml.safe_load(
@@ -47,13 +48,12 @@ for filename in os.listdir(r"./strings/langs/"):
         languages[language_name] = yaml.safe_load(
             open(r"./strings/langs/" + filename, encoding="utf8")
         )
-        # Ensure all English items exist in each language file
         for item in languages["en"]:
             if item not in languages[language_name]:
                 languages[language_name][item] = languages["en"][item]
     try:
         languages_present[language_name] = languages[language_name]["name"]
-    except KeyError:
+    except:
         print(
             "There is some issue with the language file inside bot. Please report it to the DNS NETWORK at @DNS_NETWORK on Telegram"
         )
