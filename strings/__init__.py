500da3583645cdfb3bc0cebf9bf9d10126d74ac5
AkhandaNandTripathi <168406488+PragalX@users.noreply.github.com>
2024-11-01 04:38:55 +0530

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -1,60 +1,43 @@
-#
-# Copyright (C) 2024 by IamDvis@Github, < https://github.com/IamDvis >.
-#
-# This file is part of < https://github.com/IamDvis/DV-MUSIC > project,
-# and is released under the MIT License.
-# Please see < https://github.com/IamDvis/DV-MUSIC/blob/master/LICENSE >
-#
-# All rights reserved.
-
 import os
 import sys
 from typing import List
-
 import yaml
 
 languages = {}
 commands = {}
-
 languages_present = {}
 
-
 def get_command(value: str) -> List:
-    return commands["command"][value]
-
+    try:
+        return commands["command"][value]
+    except KeyError:
+        print(f"Command '{value}' not found in commands.yml.")
+        return []
 
 def get_string(lang: str):
-    return languages[lang]
+    return languages.get(lang, languages["en"])
 
+# Load commands from commands.yml
+try:
+    with open(r"./strings/commands.yml", encoding="utf8") as f:
+        commands = yaml.safe_load(f)
+except Exception as e:
+    print(f"Error loading commands.yml: {e}")
+    sys.exit()
 
-for filename in os.listdir(r"./strings"):
-    if filename.endswith(".yml"):
-        language_name = filename[:-4]
-        commands[language_name] = yaml.safe_load(
-            open(r"./strings/" + filename, encoding="utf8")
-        )
-
-
+# Load languages from langs directory
 for filename in os.listdir(r"./strings/langs/"):
-    if "en" not in languages:
-        languages["en"] = yaml.safe_load(
-            open(r"./strings/langs/en.yml", encoding="utf8")
-        )
-        languages_present["en"] = languages["en"]["name"]
     if filename.endswith(".yml"):
         language_name = filename[:-4]
-        if language_name == "en":
-            continue
-        languages[language_name] = yaml.safe_load(
-            open(r"./strings/langs/" + filename, encoding="utf8")
-        )
-        for item in languages["en"]:
-            if item not in languages[language_name]:
-                languages[language_name][item] = languages["en"][item]
-    try:
-        languages_present[language_name] = languages[language_name]["name"]
-    except:
-        print(
-            "There is some issue with the language file inside bot. Please report it to the DNS NETWORK at @DNS_NETWORK on Telegram"
-        )
-        sys.exit()
+        try:
+            with open(r"./strings/langs/" + filename, encoding="utf8") as f:
+                languages[language_name] = yaml.safe_load(f)
+            languages_present[language_name] = languages[language_name].get("name", language_name)
+        except Exception as e:
+            print(f"There is an issue with the language file '{filename}': {e}")
+            sys.exit()
+
+# Set default language if 'en' is missing
+if "en" not in languages:
+    print("English language file is missing in langs directory.")
+    sys.exit()
